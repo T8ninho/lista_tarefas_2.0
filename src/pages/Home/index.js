7d3899a73553fdc6c745ea10dcde207790cf84df
Antonio Vitor <64814651+T8ninho@users.noreply.github.com>
2022-10-31 00:21:36 -0300

    master

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,55 +3,9 @@ import {View, Text} from 'react-native';
 
 
 export default function Filmes(){
-
-    const [searchElement, setElement] = React.useState([]);
-    const navigation = useNavigation();
-
-    const valorDigitado = '';
-
-    const addItem = (valor) => {
-        setElement([ ...searchElement, {
-            text: {valor},
-        }])
-    }
-
     return(
-        <SafeAreaView>
-            <View style={styles.container}>
-                <View style={styles.rowContainer}>
-                    <View>
-                        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                            <Feather name="chevron-left" size={25} color="#53565A" ></Feather> 
-                        </TouchableOpacity>
-                    </View>
-
-                    <View style={styles.searchBar}>
-                        <View
-                        style={{flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
-                            <Icon name="search" size={20} color="grey" />
-                            <TouchableOpacity>
-                                <TextInput 
-                                style={styles.Search}
-                                editable={true}
-                                value={valorDigitado}
-                                placeholder={"Pesquisar"}
-                                returnKeyType={'search'}
-                                onSubmitEditing={setElement(valorDigitado)}
-                                >
-                                </TextInput>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
-                {/* <View>
-                    <FlatList
-                    data={searchElement}
-                    renderItem={({item}) => {
-                        return <Text>{item}</Text>
-                    }}
-                    />
-                </View> */}
-            </View>
-        </SafeAreaView>
+        <View>
+            <Text>Eu sou um Filme</Text>
+        </View>
     )
 }
